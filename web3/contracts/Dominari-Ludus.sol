4eb263af3c7fba219542e4147de50de2991df8cf
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-05-31 12:14:11 +0300

    first commit

---
+++ web3/contracts/Dominari-Ludus.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol';
+
+conract DOMINORUM is ERC1155, Ownable, ERC1155Supply {
+  string public baseURL;
+  uint256 public constant DEVIL = 0;
+  uint256 public constant GRIFFIN = 1;
+  uint256 public constant FIREBIRD = 2;
+  uint256 public constant KAMO = 3;
+  uint256 public constant KUKULKAN = 4;
+  uint256 public constant CELESTION = 5;
+
+  uint256 public constant MAX_ATTACK_DEFEND_STRENGTH = 10;
+
+  enum BattleStatus{ PENDING, STARTED, ENDED };
+
+  
+}
