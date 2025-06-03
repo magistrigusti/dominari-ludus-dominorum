ae63f7a19144f696ba34d83126524a231ad18373
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-03 05:30:25 +0300

    0.3.7

---
+++ app/src/context/createEventListeners.js
@@ -0,0 +1,28 @@
+import { ethers } from 'ethers';
+import { ABI } from '../contract';
+
+const AddNewEvent = (eventFilter, provider, cd) => {
+  provider.removelistener(eventFilter);
+
+  provider.on(eventFilter, (logs) => {
+    const parsedLog = (new ethers.utils.Interface(ABI)).parseLog(logs);
+
+    cb(parsedLog);
+  })
+}
+
+export const createEventListeners = () => {
+  const NewPlayerEventFilter = contract.filters.NewPlayer();
+
+  AddNewEvent(NewPlayerEventFilter, provider, () => {
+    console.log('New player created!', args);
+
+    if (walletAddress === args.owner) {
+      setShowAlert({
+        status: true,
+        type: 'success',
+        message: 'Player has been successfully registered.'
+      });
+    }
+  });
+}
--- app/src/page/Home.jsx
+++ app/src/page/Home.jsx
@@ -1,54 +1,78 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { CustomButton, CustomInput, PageHOC } from '../components';
 import { useGlobalContext } from '../context';
-import { PageHOC, CustomInput, CustomButton } from '../components'
 
 const Home = () => {
-  const [playerName, setPlayerName] = useState();
-  const { contract, walletAddress, setShowAlert } = useGlobalContext();
+  const { contract, walletAddress, gameData, setShowAlert, setErrorMessage } = useGlobalContext();
+  const [playerName, setPlayerName] = useState('');
+  const navigate = useNavigate();
 
   const handleClick = async () => {
     try {
-      const playerExists = await contract.isPalyer(walletAddress);
+      const playerExists = await contract.isPlayer(walletAddress);
 
       if (!playerExists) {
-        await contract.registrPlayer(playerName, playerName);
+        await contract.registerPlayer(playerName, playerName, { gasLimit: 500000 });
 
         setShowAlert({
-          status: 'true',
+          status: true,
           type: 'info',
-          message: `${playerName} is being summoned!`
-        })
+          message: `${playerName} is being summoned!`,
+        });
+
+        setTimeout(() => navigate('/create-battle'), 8000);
       }
     } catch (error) {
-      setShowAlert({
-        status: 'true',
-        type: "error",
-        message: error.message
-      });
-      console.log(error);
-      alert(error);
+      setErrorMessage(error);
     }
-  } 
+  };
+
+  useEffect(() => {
+    const createPlayerToken = async () => {
+      const playerExists = await contract.isPlayer(walletAddress);
+      const playerTokenExists = await contract.isPlayerToken(walletAddress);
+
+      if (playerExists && playerTokenExists) navigate('/create-battle');
+    };
+
+    if (contract) createPlayerToken();
+  }, [contract]);
+
+  useEffect(() => {
+    if (gameData.activeBattle) {
+      navigate(`/battle/${gameData.activeBattle.name}`);
+    }
+  }, [gameData]);
 
   return (
-    <div className="flex flex-col" >
-      <CustomInput 
-        plaseholder="Enter your player name"
-        label="Name"
-        value={playerName}
-        handleValueChange={setPlayerName}
-      />
+    walletAddress && (
+      <div className="flex flex-col">
+        <CustomInput
+          label="Name"
+          placeHolder="Enter your player name"
+          value={playerName}
+          handleValueChange={setPlayerName}
+        />
 
-      <CustomButton 
-        title="Reister"
-        handleClick={handleClick}
-        restStyles="mt-6"
-      />
-    </div>
-  )
+        <CustomButton
+          title="Register"
+          handleClick={handleClick}
+          restStyles="mt-6"
+        />
+      </div>
+    )
+  );
 };
 
-export default PageHOC( Home,
-  <>Welcome to DOMINARIUM <br/> a Web3 NFT Card Game </>,
-  <>Connect your wallet to start playing <br/> the ultimate Web3 Battle Card Game </>
+export default PageHOC(
+  Home,
+  <>
+    Welcome to DOMINARIUM <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>,
 );
